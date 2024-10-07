99bd1799c01435ebd09e4a9351e5cc3683aebccb
EV-OD <evod599@gmail.com>
2024-10-07 15:14:13 +0545

    basic material

---
+++ src/lib/components/day-5/basicmaterial.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls, MeshLineGeometry, MeshLineMaterial } from '@threlte/extras';
+	import { Vector3, LineBasicMaterial, type Vector3Tuple, CatmullRomCurve3 } from 'three';
+	import getRandomColor from '../../../utils/colors';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[14, 14, 14]} fov={15}>
+	<OrbitControls enableZoom={false} enableDamping />
+</T.PerspectiveCamera>
+<T.AmbientLight intensity={1} />
+
+<T.Mesh position={[0, 0, 0]} rotation.x={Math.PI / -2} castShadow receiveShadow>
+	<T.PlaneGeometry args={[100, 100]} />
+	<T.MeshStandardMaterial color="gray" />
+</T.Mesh>
+
+<T.Mesh>
+    <T.BoxGeometry args={[1, 1, 1]} />
+    <T.MeshBasicMaterial color={getRandomColor()} />
+</T.Mesh>
+
+
+
+
+<T.AxesHelper args={[5]} />
--- src/lib/components/day5.svelte
+++ src/lib/components/day5.svelte
@@ -1,33 +1,9 @@
 <script lang="ts">
-	import { T } from '@threlte/core';
-	import { OrbitControls, MeshLineGeometry, MeshLineMaterial } from '@threlte/extras';
-	import { Vector3, LineBasicMaterial, type Vector3Tuple, CatmullRomCurve3 } from 'three';
-	import getRandomColor from '../../utils/colors';
-	const curve = new CatmullRomCurve3([
-    new Vector3(-3, 0, 0),
-    new Vector3(-1, 1, -1),
-    new Vector3(1, -1, 1),
-    new Vector3(3, 0, 0)
-  ])
-  const points = curve.getPoints(100)
+	import Basicmaterial from "./day-5/basicmaterial.svelte";
+	import Meshline from "./day-5/meshline.svelte";
+
+
 </script>
 
-<T.PerspectiveCamera makeDefault position={[14, 14, 14]} fov={15}>
-	<OrbitControls enableZoom={false} enableDamping />
-</T.PerspectiveCamera>
-<T.AmbientLight intensity={1} />
-
-<!-- <T.Mesh position={[0, 0, 0]} rotation.x={Math.PI / -2} castShadow receiveShadow>
-	<T.PlaneGeometry args={[100, 100]} />
-	<T.MeshStandardMaterial color="gray" />
-</T.Mesh> -->
-
-
-<T.Group scale={1}>
-	<T.Mesh>
-		<MeshLineGeometry {points} />
-		<MeshLineMaterial width={0.5} color="#fe3d00" />
-	</T.Mesh>
-</T.Group>
-
-<T.AxesHelper args={[5]} />
+<!-- <Meshline/> -->
+<Basicmaterial/>
