d10fbe49f516933b7e5ac8e9ccadae3934df0305
EV-OD <evod599@gmail.com>
2024-10-07 15:33:31 +0545

    phongmaterial

---
+++ src/lib/components/day-5/phongmaterial.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls, MeshLineGeometry, MeshLineMaterial } from '@threlte/extras';
+	import { Vector3, LineBasicMaterial, type Vector3Tuple, CatmullRomCurve3 } from 'three';
+	import getRandomColor from '../../../utils/colors';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[14, 14, 14]} fov={15}>
+	<OrbitControls enableZoom={false} enableDamping />
+</T.PerspectiveCamera>
+<T.AmbientLight intensity={0.5} />
+<T.DirectionalLight position={[10, 10, 5]} intensity={1} />
+<T.DirectionalLight position={[10, 10, -10]} intensity={1} />
+
+
+
+
+<T.Mesh position={[0, 0, 0]} rotation.x={Math.PI / -2} castShadow receiveShadow>
+	<T.PlaneGeometry args={[100, 100]} />
+	<T.MeshStandardMaterial color="gray" />
+</T.Mesh>
+
+<T.Mesh>
+    <T.PlaneGeometry args={[1, 1]} />
+    <T.MeshPhongMaterial color={getRandomColor()} side={T.DoubleSide} />
+</T.Mesh>
+
+
+
+
+<T.AxesHelper args={[5]} />
--- src/lib/components/day5.svelte
+++ src/lib/components/day5.svelte
@@ -2,10 +2,12 @@
 	import Basicmaterial from "./day-5/basicmaterial.svelte";
 	import Lambertmaterial from "./day-5/lambertmaterial.svelte";
 	import Meshline from "./day-5/meshline.svelte";
+	import Phongmaterial from "./day-5/phongmaterial.svelte";
 
 
 </script>
 
 <!-- <Meshline/> -->
 <!-- <Basicmaterial/> -->
-<Lambertmaterial/>
+<!-- <Lambertmaterial/> -->
+ <Phongmaterial/>
